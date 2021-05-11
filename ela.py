582faedd20ffa150aff8e937537d279b8cce5922
Shiv Prasad <prasadshiv1010@gmail.com>
2021-05-11 14:06:28 +0530

    Initial Commit

---
+++ ela.py
@@ -0,0 +1,33 @@
+import os, sys
+from PIL import Image, ImageChops, ImageEnhance
+
+#converts input image to ela applied image
+def convert_to_ela_image(path,quality):
+
+    original_image = Image.open(path).convert('RGB')
+
+    #resaving input image at the desired quality
+    resaved_file_name = 'resaved_image.jpg'     #predefined filename for resaved image
+    original_image.save(resaved_file_name,'JPEG',quality=quality)
+    resaved_image = Image.open(resaved_file_name)
+
+    #pixel difference between original and resaved image
+    ela_image = ImageChops.difference(original_image,resaved_image)
+    
+    #scaling factors are calculated from pixel extremas
+    extrema = ela_image.getextrema()
+    max_difference = max([pix[1] for pix in extrema])
+    if max_difference ==0:
+        max_difference = 1
+    scale = 350.0 / max_difference
+    
+    #enhancing elaimage to brighten the pixels
+    ela_image = ImageEnhance.Brightness(ela_image).enhance(scale)
+
+    ela_image.save("ela_image.png")
+    return ela_image
+
+if __name__ == "__main__":
+    file_path = sys.argv[1]
+    quality = int(sys.argv[2])
+    convert_to_ela_image(file_path,quality).show()
---
+++ ui.py
@@ -0,0 +1,62 @@
+import sys
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QDialog, QFileDialog, QLabel, QMessageBox
+from PyQt5.uic import loadUi
+from PyQt5.uic.properties import QtGui
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
+from ela import convert_to_ela_image
+from prediction import predict_result
+
+class MainWindow(QDialog):    
+    def __init__(self):
+        super(MainWindow,self).__init__()
+        loadUi("gui.ui",self)
+        self.Browse.clicked.connect(self.open_image)
+        self.Test.clicked.connect(self.result)
+        self.Quit.clicked.connect(self.close_main_window)
+       
+    def open_image(self):
+        #display original image
+        self.fname = QFileDialog.getOpenFileName(self, 'Open file', 'C:\'', ("*.png, *.xmp *.jpg"))
+        self.filename.setText(self.fname[0])
+        pixmap = QPixmap(self.fname[0])
+        self.ORIGINAL_IMAGE.setPixmap(pixmap)
+        self.ORIGINAL_IMAGE.setPixmap(pixmap.scaled(self.ORIGINAL_IMAGE.size(),Qt.IgnoreAspectRatio))
+        self.ORIGINAL_IMAGE.show()
+
+        #display ela image
+        convert_to_ela_image(self.fname[0],90)
+        pixmap1 = QPixmap("ela_image.png")
+        self.ELA_IMAGE.setPixmap(pixmap1)
+        self.ELA_IMAGE.setPixmap(pixmap1.scaled(self.ELA_IMAGE.size(),Qt.IgnoreAspectRatio))
+        self.ELA_IMAGE.show()   
+
+    def result(self):
+        (prediction,confidence) = predict_result((self.fname))
+        self.Result.setText(f'Prediction: {prediction}\nConfidence: {confidence} %') 
+
+    def close_main_window(self):
+        #quit window
+        reply = QMessageBox.question(self, "Quit", "Are you sure you want to quit?", QMessageBox.Cancel | QMessageBox.Close)
+        if reply == QMessageBox.Close:
+            sys.exit()
+    
+
+def main():
+    app = QApplication(sys.argv)
+    Result = MainWindow()
+    widget = QtWidgets.QStackedWidget()
+    widget.addWidget(Result)
+    widget.setFixedWidth(620)
+    widget.setFixedHeight(560)
+    widget.show()
+    sys.exit(app.exec_())
+
+if __name__== "__main__":
+    main()
+
+
+        
+     
+
